9ccd82442f40a087aaa649949058720494772fa7
Lucas <lucas.fabreguettes@epitech.eu>
2019-04-08 16:13:46 +0200

    [ADD] Adding basis of creatinf of graph

---
+++ solver/get_maze/get_maze.c
@@ -0,0 +1,95 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_dante_2018
+** File description:
+** get_maze
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "maze.h"
+#include "my.h"
+#include "graph.h"
+
+char **copy_map(char *str, char **map)
+{
+    int cols = 0;
+    int rows = 0;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\n') {
+            map[rows][cols] = '\0';
+            rows++;
+            cols = 0;
+        }
+        else {
+            map[rows][cols] = str[i];
+            cols++;
+        }
+    }
+    map[rows] = NULL;
+    free(str);
+    return (map);
+}
+
+char **create_map(char *str, maze_t *maze)
+{
+    int i = 0;
+    char **map = NULL;
+
+    maze->cols = count_cols(str);
+    maze->rows = count_rows(str);
+    maze->size = maze->cols * maze->rows;
+    map = malloc(sizeof(char *) * (maze->rows + 1));
+    if (map == NULL)
+        return (NULL);
+    for (; i < maze->rows; i++) {
+        map[i] = malloc(sizeof(char) * (maze->cols + 1));
+        if (map[i] == NULL)
+            return (NULL);
+    }
+    map[i] = NULL;
+    map = copy_map(str, map);
+    return (map);
+}
+
+char *get_file(char **argv)
+{
+    struct stat my_stat;
+    int length = 0;
+    char *str = NULL;
+    int size = 0;
+    int fd = 0;
+
+    stat(argv[1], &my_stat);
+    length = my_stat.st_size;
+    str = malloc(sizeof(char) * (length + 1));
+    fd = open(argv[1], O_RDONLY);
+    size = read(fd, str, length);
+    if (size == -1) {
+        free(str);
+        exit(84);
+    }
+    str[length] = '\0';
+    return (str);
+}
+
+void create_maze(char **argv)
+{
+    char *file = NULL;
+    maze_t *maze = malloc(sizeof(maze_t));
+
+    file = get_file(argv);
+    maze->map = create_map(file, maze);
+    /* for (int i = 0; maze->map[i] != NULL; i++)
+        printf("%s\n", maze->map[i]); */
+    /* for (int i = 0; maze->map[i] != NULL; i++)
+        free(maze->map[i]);
+    free(maze->map);
+    free(maze); */
+    create_graph(maze);
+}
---
+++ solver/include/my.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_dante_2018
+** File description:
+** my
+*/
+
+#ifndef MY_H_
+#define MY_H_
+
+char *get_file(char **argv);
+void create_maze(char **argv);
+int count_rows(char *str);
+int count_cols(char *str);
+#endif /* !MY_H_ */
