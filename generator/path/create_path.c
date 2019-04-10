69a45ede2f6b1f3002e1dd6c0a0dcb79503fb881
Lucas <lucas.fabreguettes@epitech.eu>
2019-04-10 11:57:48 +0200

    [~] create_path To create a really perfect maze

--- generator/path/create_path.c
+++ generator/path/create_path.c
@@ -14,18 +14,15 @@ char **handle_random(char **map, int *i, int *j)
 
     cond = rand() % 2;
     map[*i][*j] = '*';
-    if (cond == 0) {
-        if (*j - 1 > 0) {
-            map[*i][*j - 1] = '*';
-            map[*i][*j - 2] = '*';
+    if (cond == 0 && *j > 1) {
+        for (int count = 0; *j - count >= 0 && count < 2; count++) {
+            map[*i][*j - count] = '*';
         }
-        *j -= 2;
     }
-    if (cond == 1) {
+    if (cond == 1 || *j <= 1) {
         if (*i - 1 >= 0) {
             map[*i - 1][*j] = '*';
             map[*i - 2][*j] = '*';
-            (*j) -= 2;
         }
     }
     return (map);
@@ -34,19 +31,19 @@ char **handle_random(char **map, int *i, int *j)
 char **create_path(char **map, int x, int y)
 {
     int i = y - 1;
-    int j = x - 1;
+    int min = 0;
 
     if (y % 2 == 0)
         i -= 1;
+    if (x % 2 == 0)
+        min = 1;
+    else
+        min = 0;
     for (; i >= 0; i -= 2) {
-        while (j >= 0) {
+        for (int j = x - 1; j >= min; j-=2)
             map = handle_random(map, &i, &j);
-        }
-        j = x - 1;
     }
     for (int f = x - 1; f >= 0; f--)
         map[0][f] = '*';
-/*     for (int f = y - 1; f >= 0; f--)
-        map[f][0] = '*'; */
     return (map);
 }
